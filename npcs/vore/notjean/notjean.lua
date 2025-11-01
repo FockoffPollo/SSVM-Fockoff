ae9d4beeced8b97e8a1979aad1d173f6739f9223
FockoffPollo <perplestuph@gmail.com>
2025-11-01 16:29:36 +0800

    Not Jean NPC test
    Sadly I wasn't able to get the SBQ version of this working so to compensate I reused some of the assets from that to make this. Currently there's no dialogue (It's the default one you see with the fox and wolves.) I made add some later.

---
+++ npcs/vore/notjean/notjean.lua
@@ -0,0 +1,53 @@
+require "/scripts/vore/npcvore.lua"
+
+capacity = 2
+
+playerLines = {		""
+}
+
+function initHook()
+
+	index = npc.getItemSlot("legs").parameters.colorIndex
+
+	legs[2] = {
+		name = "notjeanlegsbelly1",
+		parameters = {
+					colorIndex = index
+	}}
+	chest[2] = {
+		name = "notjeanchest1",
+		parameters = {
+					colorIndex = index
+	}}
+	
+	legs[3] = {
+		name = "notjeanlegsbelly2",
+		parameters = {
+					colorIndex = index
+	}}
+	chest[3] = {
+		name = "notjeanchestbelly2",
+		parameters = {
+					colorIndex = index
+	}}
+
+end
+
+function feedHook()
+	world.spawnProjectile( "npcanimchomp" , world.entityPosition( tempTarget ), entity.id(), {0, 0}, false)
+	world.spawnProjectile( "swallowprojectile" , world.entityPosition( tempTarget ), entity.id(), {0, 0}, false)
+end
+
+function requestHook(args)
+	world.spawnProjectile( "npcanimchomp" , world.entityPosition( victim[#victim] ), entity.id(), {0, 0}, false)
+	world.spawnProjectile( "swallowprojectile" , world.entityPosition( victim[#victim] ), entity.id(), {0, 0}, false)
+end
+
+function updateHook()
+
+	if containsPlayer() and math.random(700) == 1 and ( playerTimer < duration or request[1] == true or request[2] == true ) then
+		sayline( playerLines )
+	end
+
+end
+
